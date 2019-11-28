6f24095174ef4841e9f11558c525d12d1184fdc9
rubychan493 <rubychan493@yahoo.com>
2019-11-29 01:49:17 +0800

    29 Nov 2019

---
+++ javaScript/script.js
@@ -0,0 +1,8 @@
+function time() {
+	moment.locale("zh-hk");
+	var now = moment(new Date()); //todays date
+	var end = moment("2018-12-27"); // another date
+	var duration = moment.duration(now.diff(end));
+	var days = duration.asDays();
+	document.getElementById("timefromaday").innerHTML = "我和悉瑜在一起已經 "+Math.ceil(days)+" 天";
+}
