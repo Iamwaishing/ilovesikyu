3c25653222feb7272ffa5729804dea5a66b65d2c
IamWaishing <waishing2702@gmail.com>
2019-11-29 01:56:10 +0800

    29 Nov 2019

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,6 @@
-function time() {
-	moment.locale("zh-hk");
-	var now = moment(new Date()); //todays date
-	var end = moment("2018-12-27"); // another date
-	var duration = moment.duration(now.diff(end));
-	var days = duration.asDays();
-	document.getElementById("timefromaday").innerHTML = "我和悉瑜在一起已經 "+Math.ceil(days)+" 天";
-}
+moment.locale("zh-hk"); //change to Hong Kong Time
+var now = moment(new Date()); //todays date
+var end = moment("2018-12-27"); // another date
+var duration = moment.duration(now.diff(end));
+var days = duration.asDays();
+document.getElementById("timefromaday").innerHTML = "我和悉瑜在一起已經 "+Math.ceil(days)+" 天";
